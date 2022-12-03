053365ca86d13086fb5795964e01c44218cbbcf2
Saeid <saeidtayebinasab@gmail.com>
2022-12-03 19:01:02 +0330

    got rid of Card and InkWell

--- lib/notes_list_item.dart
+++ lib/notes_list_item.dart
@@ -14,45 +14,42 @@ class NotesListItem extends StatelessWidget {
   Widget build(BuildContext context) {
     return Container(
       height: 70,
-      child: InkWell(
-        onTap: () {},
-        highlightColor: Colors.orange,
-        splashColor: Colors.black,
-        focusColor: Colors.orange,
-        hoverColor: Colors.blue,
-        child: Card(
-          //color: Color.fromARGB(255, 255, 249, 194),
-          elevation: 5,
-          child: Container(
-            decoration: const BoxDecoration(
-              gradient: LinearGradient(
-                begin: Alignment.topCenter,
-                end: Alignment.bottomCenter,
-                colors: [Color(0xFFFDF6DD), Color(0xFFFEF1C6)],
-              ),
-            ),
-            child: Row(
-              mainAxisAlignment: MainAxisAlignment.spaceBetween,
-              children: [
-                Container(
-                  width: 300,
-                  child: Text(
-                    shortText,
-                    style: TextStyle(fontSize: 18),
-                    overflow: TextOverflow.ellipsis,
-                  ),
-                ),
-                Text(
-                  shortDate.toPersianDate().toString(),
-                  style: TextStyle(
-                    fontSize: 20,
-                    color: Colors.grey,
-                  ),
-                ),
-              ],
+      padding: EdgeInsets.all(3),
+      margin: EdgeInsets.symmetric(horizontal: 0, vertical: 3),
+      decoration: BoxDecoration(
+        boxShadow: [
+          BoxShadow(
+            blurRadius: 25,
+            color: Colors.grey.withOpacity(0.5),
+            spreadRadius: 7,
+            offset: Offset(0, 3),
+          ),
+        ],
+        gradient: LinearGradient(
+          begin: Alignment.topCenter,
+          end: Alignment.bottomCenter,
+          colors: [Color(0xFFFDF6DD), Color(0xFFFEF1C6)],
+        ),
+      ),
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        children: [
+          Container(
+            width: 300,
+            child: Text(
+              shortText,
+              style: TextStyle(fontSize: 18),
+              overflow: TextOverflow.ellipsis,
             ),
           ),
-        ),
+          Text(
+            shortDate.toPersianDate().toString(),
+            style: TextStyle(
+              fontSize: 20,
+              color: Colors.grey,
+            ),
+          ),
+        ],
       ),
     );
   }
