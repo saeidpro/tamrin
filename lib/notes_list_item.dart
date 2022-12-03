0af28ace34b5f6f51c49efbb23888e957cc8b342
Saeid <saeidtayebinasab@gmail.com>
2022-12-03 18:42:26 +0330

    adding style to Cards

--- lib/notes_list_item.dart
+++ lib/notes_list_item.dart
@@ -12,21 +12,47 @@ class NotesListItem extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Card(
-      color: Color.fromARGB(255, 255, 249, 194),
-      elevation: 5,
-      child: Row(
-        mainAxisAlignment: MainAxisAlignment.spaceBetween,
-        children: [
-          Expanded(child: Text(shortText, style: TextStyle(fontSize: 20))),
-          Text(
-            shortDate.toPersianDate().toString(),
-            style: TextStyle(
-              fontSize: 20,
-              color: Colors.grey,
+    return Container(
+      height: 70,
+      child: InkWell(
+        onTap: () {},
+        highlightColor: Colors.orange,
+        splashColor: Colors.black,
+        focusColor: Colors.orange,
+        hoverColor: Colors.blue,
+        child: Card(
+          //color: Color.fromARGB(255, 255, 249, 194),
+          elevation: 5,
+          child: Container(
+            decoration: const BoxDecoration(
+              gradient: LinearGradient(
+                begin: Alignment.topCenter,
+                end: Alignment.bottomCenter,
+                colors: [Color(0xFFFDF6DD), Color(0xFFFEF1C6)],
+              ),
+            ),
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                Container(
+                  width: 300,
+                  child: Text(
+                    shortText,
+                    style: TextStyle(fontSize: 18),
+                    overflow: TextOverflow.ellipsis,
+                  ),
+                ),
+                Text(
+                  shortDate.toPersianDate().toString(),
+                  style: TextStyle(
+                    fontSize: 20,
+                    color: Colors.grey,
+                  ),
+                ),
+              ],
             ),
           ),
-        ],
+        ),
       ),
     );
   }
