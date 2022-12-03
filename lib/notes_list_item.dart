dfe7c295e13bf7aea41331e80737c3422fa1b7fd
Saeid <saeidtayebinasab@gmail.com>
2022-12-03 20:03:52 +0330

    fixing the InkWell issue by using Ink instead of Container. I had to wrap Ink with InkWell, by wrap InkWell with a container to add margin

--- lib/notes_list_item.dart
+++ lib/notes_list_item.dart
@@ -13,43 +13,51 @@ class NotesListItem extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Container(
-      height: 70,
-      padding: EdgeInsets.all(3),
       margin: EdgeInsets.symmetric(horizontal: 0, vertical: 3),
-      decoration: BoxDecoration(
-        boxShadow: [
-          BoxShadow(
-            blurRadius: 25,
-            color: Colors.grey.withOpacity(0.5),
-            spreadRadius: 7,
-            offset: Offset(0, 3),
-          ),
-        ],
-        gradient: LinearGradient(
-          begin: Alignment.topCenter,
-          end: Alignment.bottomCenter,
-          colors: [Color(0xFFFDF6DD), Color(0xFFFEF1C6)],
-        ),
-      ),
-      child: Row(
-        mainAxisAlignment: MainAxisAlignment.spaceBetween,
-        children: [
-          Container(
-            width: 300,
-            child: Text(
-              shortText,
-              style: TextStyle(fontSize: 18),
-              overflow: TextOverflow.ellipsis,
+      child: InkWell(
+        onTap: () {},
+        //splashColor: Colors.orange,
+        highlightColor: Color(0xFFF8D8A9),
+        child: Ink(
+            height: 70,
+            padding: EdgeInsets.all(3),
+            //margin: EdgeInsets.symmetric(horizontal: 0, vertical: 3),
+            decoration: BoxDecoration(
+              boxShadow: [
+                BoxShadow(
+                  blurRadius: 25,
+                  color: Colors.grey.withOpacity(0.5),
+                  spreadRadius: 7,
+                  offset: Offset(0, 3),
+                ),
+              ],
+              gradient: LinearGradient(
+                begin: Alignment.topCenter,
+                end: Alignment.bottomCenter,
+                colors: [Color(0xFFFDF6DD), Color(0xFFFEF1C6)],
+              ),
             ),
+            child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  Container(
+                    width: 300,
+                    child: Text(
+                      shortText,
+                      style: TextStyle(fontSize: 18),
+                      overflow: TextOverflow.ellipsis,
+                    ),
+                  ),
+                  Text(
+                    shortDate.toPersianDate().toString(),
+                    style: TextStyle(
+                      fontSize: 20,
+                      color: Colors.grey,
+                    ),
+                  ),
+                ],
+              ),
           ),
-          Text(
-            shortDate.toPersianDate().toString(),
-            style: TextStyle(
-              fontSize: 20,
-              color: Colors.grey,
-            ),
-          ),
-        ],
       ),
     );
   }
